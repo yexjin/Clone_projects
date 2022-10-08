7021855245494de80b99fd4f46ed6b8fbb4b7fcc
yexjin <tnska0421@naver.com>
2022-10-08 10:57:00 +0900

    settings

---
+++ TossBenefits/TossBenefits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TossBenefits
+//
+//  Created by 오예진 on 2022/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
